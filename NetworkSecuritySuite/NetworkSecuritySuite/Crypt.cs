99fb315182f94be9bb835e724fa34fd39c334aa9
MJB52 <42855565+MJB52@users.noreply.github.com>
2018-09-15 17:55:37 -0500

    Update Crypt.cs
    comments

--- NetworkSecuritySuite/NetworkSecuritySuite/Crypt.cs
+++ NetworkSecuritySuite/NetworkSecuritySuite/Crypt.cs
@@ -18,7 +18,7 @@ namespace NetworkSecuritySuite
         }
        //                                                    a     b     c     d      e     f    g     h     i     j   k    l     m     n     o     p     q     r    s     t     u      v    w    x     y     z    
         private static readonly double[] FrequencyLookup = { 8.12, 1.49, 2.71, 4.32, 12.02, 2.3, 2.03, 5.92, 7.31, .1, .69, 3.98, 2.61, 6.95, 7.68, 1.82, .11, 6.02, 6.28, 9.10, 2.88, 1.11, 2.09, .17, 2.11, .07};
-
+        //this ^^ contains frequency for each letter in english language
         public static string Decrypt(string message, string key)
         {
             int[] charMessage = ConvertToInt(message.ToLower());
@@ -92,25 +92,25 @@ namespace NetworkSecuritySuite
             int count = 0;
             int keyCount = 0;
             //int letterCount = 0;
-            string[] keysColumn = new string[KeyLength];
-            string[] likelyKeys = new string[3];
-            string[] column = CreateColumns(CipherText, KeyLength);
-            string[] decrypted = new string[26];
+            string[] keysColumn = new string[KeyLength];    
+            string[] likelyKeys = new string[3];        //holds suggested keys
+            string[] column = CreateColumns(CipherText, KeyLength); //gets ciphertext into specified columns
+            string[] decrypted = new string[26];        //holds each decrypted line 
             foreach(string line in column)
             {
                 for(char letter = 'a'; letter <= 'z'; letter++)
                 { 
-                    decrypted[count] = Decrypt(line, letter.ToString());
+                    decrypted[count] = Decrypt(line, letter.ToString());    //decrypts column26 times for each letter
                     count++; 
                 }
-                keysColumn[keyCount] = GetLikelyKeys(decrypted);
+                keysColumn[keyCount] = GetLikelyKeys(decrypted);    //send array of decrypted strings to get likely keys
                 Console.WriteLine(keysColumn[keyCount].ToString());
                 keyCount++;
                 count = 0;
             }
             for (int i = 0; i < likelyKeys.Length; i++)
             {
-                likelyKeys[i] += (keysColumn[0][i] + keysColumn[1][i].ToString() + keysColumn[2][i].ToString() + keysColumn[3][i].ToString() + keysColumn[4][i].ToString()).ToUpper(); //assign ith char to likelykeys
+                likelyKeys[i] += (keysColumn[0][i] + keysColumn[1][i].ToString() + keysColumn[2][i].ToString() + keysColumn[3][i].ToString() + keysColumn[4][i].ToString()).ToUpper(); //assign Ith char to likelykeys
             }
             foreach(string key in likelyKeys)
                 Console.WriteLine(key);
@@ -123,14 +123,15 @@ namespace NetworkSecuritySuite
             //int[] MessageAsInt = new int[message[0].Length];
             //double[] total = new double[message.Length];
             char[] arr = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-            double freq,total1 = 0, total2 = 0, total3 = 0;
+            double freq;
+            double total1 = 0 /*most likely key for this column*/, total2 = 0/*2nd most likely*/, total3 = 0;/*3rd most likely*/
             int count = 0;
             //int linecount = 0;
             foreach(string line in message)
             {
                 //MessageAsInt = ConvertToInt(line);
                 // total[count] = FrequencyAnalysis(line);
-                freq = FrequencyAnalysis(line);
+                freq = FrequencyAnalysis(line); //just gets double value based on the line
                 if (freq > total1)
                 {
                     total3 = total2;
